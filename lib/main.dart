0b6a7e499e272c1d4eef49b96845f183715f80ae
Areeba Iqbal <areeba.seo05@gmail.com>
2025-01-05 18:58:19 +0500

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -26,8 +26,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
-       // debugShowCheckedModeBanner: false,
+       //debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
